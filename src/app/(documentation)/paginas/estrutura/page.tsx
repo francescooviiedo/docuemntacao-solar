d1074799fd40f1016a3945b7bf1a203b3bf0d27b
Francesco Oviedo <francesco.oviedo@defensoria.mg.def.br>
2025-05-12 13:49:35 -0300

    pagina estrutura do codigo

--- src/app/(documentation)/paginas/estrutura/page.tsx
+++ src/app/(documentation)/paginas/estrutura/page.tsx
@@ -1,19 +1,16 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Divider, Typography } from '@mui/material';
 import * as React from 'react';
 import CopyToClipboard from '../../../components/CopyPaste';
+import { codeSnippets } from '../validation/codeSnippets';
+import FolderStructure from '../../../components/Folderstructure';
 export default function StructurePage() {
   return (
     <Box sx={{ padding: '16px' }}>
-      <Typography variant="body1" sx={{ marginBottom: '16px' }}>
-        Nesta sessao vamos explicar a estrutura do projeto e como ele foi organizado. O projeto é dividido em pastas principais, cada uma com sua própria função. Aqui estão as principais pastas:
-      </Typography>
-      <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: '16px' }}>
-        <CopyToClipboard content={`<Tooltip title={copied ? "Copied!" : "Copy"}>
-  <Button onClick={handleCopy} sx={{ height: 10, width: 10, borderRadius: 1 }}>
-      <ContentCopyIcon />
-  </Button>
-</Tooltip>`}
-      />
+      <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: '1px', flexDirection: 'column' }}>
+        <Typography variant="h4" sx={{ marginBottom: '1px', marginTop: '16px' }}>
+          <strong>Estrutura de pastas:</strong>
+        </Typography>
+        <FolderStructure />
       </Box>
     </Box>
   );
